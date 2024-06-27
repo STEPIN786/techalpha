5a9238ce198e6154188597ed1b03566410c6c00f
Juli786 <juli.dhola@trindia.in>
2024-06-27 15:10:11 +0530

    first commit

--- routes/admin.php
+++ routes/admin.php
@@ -24,12 +24,12 @@ use Illuminate\Support\Facades\Route;
     Route::get('/', function () { return redirect()->route('login'); });
     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [LoginController::class, 'login']);
-    Route::get('/register', function () { return view('pages.register'); });
+    Route::get('/register', function () { return view('admin.register'); });
     Route::post('/register', [RegisterController::class, 'register']);
 
 
 
-    // Route::get('password/forget',  function () { return view('pages.forgot-password'); })->name('password.forget');
+    Route::get('password/forget',  function () { return view('admin.forgot-password'); })->name('password.forget');
 
 
 
