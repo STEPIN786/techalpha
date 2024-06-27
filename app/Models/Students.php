d76d1a1eaa4c27f663690135aee025a64e88cf05
Juli786 <juli.dhola@trindia.in>
2024-06-27 11:39:19 +0530

    first commit

--- app/Models/Students.php
+++ app/Models/Students.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Students extends Model
 {
     use HasFactory;
+    protected $table = 'students';
+    protected $fillable = ['name','course','center','complition_date','registration_number','student_image','status','created_at','updated_at'];
 }
