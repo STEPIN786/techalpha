58058820b642c3ee33f60d6ded71a6199a4fdab8
Juli786 <juli.dhola@trindia.in>
2024-07-12 15:20:43 +0530

    first commit

--- resources/views/include/script.blade.php
+++ resources/views/include/script.blade.php
@@ -4,3 +4,32 @@
 
 <script src="{{ asset('dist/js/theme.js') }}"></script>
 <script src="{{ asset('js/chat.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.20.0/jquery.validate.min.js"></script>
+    <script>
+         $("#my_form").validate({
+            rules: {
+                name: { required: true },
+                message: { required: true },
+                phone: { required: true, minlength: 10 },
+                
+                
+            },
+           
+            errorElement: 'span',
+            errorPlacement: function(error, element) {
+                error.addClass('invalid-feedback');
+                element.closest('.elem').append(error);
+            },
+            highlight: function(element, errorClass, validClass) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                $(element).removeClass('is-invalid');
+            },
+           
+            submitHandler: function(form) {
+                // do other things for a valid form
+                form.submit();
+            }
+        });
+    </script>
